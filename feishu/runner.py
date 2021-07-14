1a50a1d6b620c80e30cd8c0a0d95e766554c1b3d
lz <zerocen>
2021-07-14 22:31:59 +0800

    Update runner code

--- feishu/runner.py
+++ feishu/runner.py
@@ -1,4 +1,15 @@
-class Runner:
+import pytest
 
-    def run_test(self):
-        pass
+
+class Runner:
+    web_test_case_dir = "web_test/test_case"
+    api_test_case_dir = "api_test/test_case"
+
+    @classmethod
+    def run_test(cls, test_case_path):
+        pytest.main(["-sv", test_case_path])
+
+
+if __name__ == '__main__':
+    # Runner.run_test(Runner.web_test_case_dir)
+    Runner.run_test(Runner.api_test_case_dir)
